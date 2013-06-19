001daba1e5d8a2bf8946865fc675df7ccab722e4
hugoiuri <hugoraiuri@gmail.com>
2013-06-18 22:50:21 -0300

    Initial template of queue list page

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -11,9 +11,18 @@ namespace MsmqManager.Web
             bundles.Add(new ScriptBundle("~/Scripts/bootstrap")
                 .Include("~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/Scripts/jquery.tablesorter")
+                .Include("~/Scripts/jquery.tablesorter.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js"));
+
             bundles.Add(new StyleBundle("~/Content/bootstrap")
                 .Include("~/Content/bootstrap/bootstrap.css")
                 .Include("~/Content/bootstrap/bootstrap-responsive.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/site.css"));
         }
     }
 }
--- Web/Controllers/QueueListController.cs
+++ Web/Controllers/QueueListController.cs
@@ -16,9 +16,25 @@ namespace MsmqManager.Web.Controllers
         {
             List<QueueModel> model = new List<QueueModel>();
 
-            model.Add(new QueueModel("a", 1000000));
-            model.Add(new QueueModel("b", 100));
-            model.Add(new QueueModel("c", 1000));
+            model.Add(new QueueModel("adsaj kj hsldaighkf A", 1000000));
+            model.Add(new QueueModel("bSAD SHG KAG SFKSBF SA CCasf nkjbvs fm", 100));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 1000));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 999));
+            model.Add(new QueueModel("alksfjsabfgal çkb", 999));
+            model.Add(new QueueModel("keflawinfclaisg shld abv ", 0));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 34));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 13));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 25));
+            model.Add(new QueueModel("bnmbnmbnmbnmnb çkb", 10));
+            model.Add(new QueueModel("cdecde çkb", 10));
+            model.Add(new QueueModel("cdecde çkb", 0));
+            model.Add(new QueueModel("asdadasdass çkb", 10));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 10));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 47));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 53));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 21));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 78));
+            model.Add(new QueueModel("dfgsjkgbsdkfj çkb", 0));
 
             return View(model);
         }
